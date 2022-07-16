bebeea0b6015343868d0fa6fdd742ad4d1fe1eed
VSartz <elsartz@gmail.com>
2022-07-15 21:14:45 -0400

    first theory

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,17 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,6 +30,7 @@ const LoginForm = () => {
 
     try {
       const response = await loginUser(userFormData);
+      console.log('response:',response);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
